843b0eccf607cd515ea36a9867a1b65cdd26ad87
BenRaz123 <ben.raz2008@gmail.com>
2023-07-10 12:48:24 -0400

    Formatted Files
    Signed-off-by: BenRaz123 <ben.raz2008@gmail.com>

--- src/fileutils.rs
+++ src/fileutils.rs
@@ -1,74 +1,75 @@
-    use std::io::prelude::*;
-    pub fn file_is_empty(path: &str) -> bool {
-        let file = std::fs::OpenOptions::new().read(true).open(path).unwrap();
-        let reader = std::io::BufReader::new(&file);
-        let buf: String = reader
-            .lines()
-            .map(|l| l.unwrap())
-            .collect::<Vec<String>>()
-            .join("");
-        match &buf[..] {
-            "" | " " | "\n" => true,
-            _ => false,
-        }
+
+use std::io::prelude::*;
+pub fn file_is_empty(path: &str) -> bool {
+    let file = std::fs::OpenOptions::new().read(true).open(path).unwrap();
+    let reader = std::io::BufReader::new(&file);
+    let buf: String = reader
+        .lines()
+        .map(|l| l.unwrap())
+        .collect::<Vec<String>>()
+        .join("");
+    match &buf[..] {
+        "" | " " | "\n" => true,
+        _ => false,
+    }
+}
+
+pub fn get_file_path() -> String {
+    let home = std::env::var("HOME");
+    if home.is_err() {
+        eprintln!("error: $HOME is not set!");
+        std::process::exit(1);
+    }
+    format!("{}/secrets.txt", home.unwrap())
+}
+
+pub fn write_to_file(input: &str) {
+    let mut file = std::fs::OpenOptions::new()
+        .read(true)
+        .create(true)
+        .append(true)
+        .open(get_file_path())
+        .unwrap();
+    writeln!(file, "{input}").unwrap();
+}
+
+pub fn read_from_file() -> Option<Vec<String>> {
+    let file = std::fs::OpenOptions::new().read(true).open(get_file_path());
+
+    if file.is_err() {
+        return None;
     }
 
-    pub fn get_file_path() -> String {
-        let home = std::env::var("HOME");
-        if home.is_err() {
-            eprintln!("error: $HOME is not set!");
-            std::process::exit(1);
-        }
-        format!("{}/secrets.txt", home.unwrap())
-    }
+    let mut buff = String::new();
 
-    pub fn write_to_file(input: &str) {
-        let mut file = std::fs::OpenOptions::new()
-            .read(true)
-            .create(true)
-            .append(true)
-            .open(get_file_path())
-            .unwrap();
-        writeln!(file, "{input}").unwrap();
-    }
+    file.unwrap().read_to_string(&mut buff).unwrap();
 
-    pub fn read_from_file() -> Option<Vec<String>> {
-        let file = std::fs::OpenOptions::new().read(true).open(get_file_path());
+    let output: Vec<&str> = buff.split("\n").collect::<Vec<&str>>();
 
-        if file.is_err() {
-            return None;
-        }
+    let mut owned_output: Vec<String> = Vec::new();
 
-        let mut buff = String::new();
+    output.iter().for_each(|x| owned_output.push(x.to_string()));
 
-        file.unwrap().read_to_string(&mut buff).unwrap();
+    Some(owned_output)
+}
 
-        let output: Vec<&str> = buff.split("\n").collect::<Vec<&str>>();
+pub fn remove_index_from_file(index: u32) -> Result<(), Box<dyn std::error::Error>> {
+    let file_read = std::fs::OpenOptions::new()
+        .read(true)
+        .open(get_file_path())?;
 
-        let mut owned_output: Vec<String> = Vec::new();
+    let reader = std::io::BufReader::new(&file_read);
 
-        output.iter().for_each(|x| owned_output.push(x.to_string()));
+    let mut buf: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
 
-        Some(owned_output)
-    }
+    buf.remove(index as usize);
 
-    pub fn remove_index_from_file(index: u32) -> Result<(), Box<dyn std::error::Error>> {
-        let file_read = std::fs::OpenOptions::new()
-            .read(true)
-            .open(get_file_path())?;
+    let mut file_write = std::fs::OpenOptions::new()
+        .truncate(true)
+        .write(true)
+        .open(get_file_path())?;
 
-        let reader = std::io::BufReader::new(&file_read);
+    file_write.write_all(format!("{}\n", buf.join("\n")).as_bytes())?;
 
-        let mut buf: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
-
-        buf.remove(index as usize);
-
-        let mut file_write = std::fs::OpenOptions::new()
-            .truncate(true)
-            .write(true)
-            .open(get_file_path())?;
-
-        file_write.write_all(format!("{}\n", buf.join("\n")).as_bytes())?;
-
-        Ok(())
-    }
+    Ok(())
+}
--- src/passutils.rs
+++ src/passutils.rs
@@ -1,74 +1,74 @@
 
-    use std::io::prelude::*;
+use std::io::prelude::*;
 
-    macro_rules! prompt_pass {
-        ($name:ident with message $message:expr) => {
-            let question = requestty::Question::password("$name")
-                .message($message.to_string())
-                .mask('*')
-                .build();
-            let answer = requestty::prompt_one(question).unwrap();
-            let $name = answer.as_string().unwrap().to_string();
-        };
+macro_rules! prompt_pass {
+    ($name:ident with message $message:expr) => {
+        let question = requestty::Question::password("$name")
+            .message($message.to_string())
+            .mask('*')
+            .build();
+        let answer = requestty::prompt_one(question).unwrap();
+        let $name = answer.as_string().unwrap().to_string();
+    };
+}
+
+fn pass_is_empty() -> bool {
+    let pass_file = std::fs::OpenOptions::new().read(true).open("./pass.txt");
+    if pass_file.is_err() {
+        return true;
+    }
+    let mut pass = String::new();
+    pass_file.unwrap().read_to_string(&mut pass).unwrap();
+    if pass.trim().is_empty() {
+        return true;
+    }
+    false
+}
+
+fn new_password() -> Result<(), Box<dyn std::error::Error>> {
+    prompt_pass!(new_pass with message "Please enter a new password");
+    prompt_pass!(rep_pass with message "Please repeat that password");
+
+    if rep_pass != new_pass {
+        println!("The two passwords do not match!");
+        std::process::exit(1);
     }
 
-    fn pass_is_empty() -> bool {
-        let pass_file = std::fs::OpenOptions::new().read(true).open("./pass.txt");
-        if pass_file.is_err() {
-            return true;
-        }
-        let mut pass = String::new();
-        pass_file.unwrap().read_to_string(&mut pass).unwrap();
-        if pass.trim().is_empty() {
-            return true;
-        }
-        false
+    let mut pass_file = std::fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open("./pass.txt")?;
+
+    pass_file.write_all(new_pass.as_bytes())?;
+
+    Ok(())
+}
+
+fn check_password(pass: &str) {
+    let mut pass_file = std::fs::OpenOptions::new()
+        .read(true)
+        .open("./pass.txt")
+        .expect("Password file not found");
+    let mut official_pass = String::new();
+    pass_file
+        .read_to_string(&mut official_pass)
+        .expect("Could not read password file");
+    if pass != official_pass.trim() {
+        eprintln!("Incorrect password!");
+        std::process::exit(1)
     }
+}
 
-    fn new_password() -> Result<(), Box<dyn std::error::Error>> {
-        prompt_pass!(new_pass with message "Please enter a new password");
-        prompt_pass!(rep_pass with message "Please repeat that password");
+fn prompt_password() -> String {
+    prompt_pass!(pass with message "Please enter your password");
+    pass
+}
 
-        if rep_pass != new_pass {
-            println!("The two passwords do not match!");
-            std::process::exit(1);
-        }
-
-        let mut pass_file = std::fs::OpenOptions::new()
-            .write(true)
-            .truncate(true)
-            .create(true)
-            .open("./pass.txt")?;
-
-        pass_file.write_all(new_pass.as_bytes())?;
-
-        Ok(())
+pub fn authenticate() {
+    if pass_is_empty() {
+        new_password().unwrap();
+        return;
     }
-
-    fn check_password(pass: &str) {
-        let mut pass_file = std::fs::OpenOptions::new()
-            .read(true)
-            .open("./pass.txt")
-            .expect("Password file not found");
-        let mut official_pass = String::new();
-        pass_file
-            .read_to_string(&mut official_pass)
-            .expect("Could not read password file");
-        if pass != official_pass.trim() {
-            eprintln!("Incorrect password!");
-            std::process::exit(1)
-        }
-    }
-
-    fn prompt_password() -> String {
-        prompt_pass!(pass with message "Please enter your password");
-        pass
-    }
-
-    pub fn authenticate() {
-        if pass_is_empty() {
-            new_password().unwrap();
-            return;
-        }
-        check_password(&prompt_password());
-    }
+    check_password(&prompt_password());
+}
