a8d41487b0038a7c3a7b528bfb5228012b41207a
BenRaz123 <ben.raz2008@gmail.com>
2023-07-10 12:32:08 -0400

    Sideloaded functions to external modules
    Moved file_is_empty(), get_file_path(), read_from_file(), remove_index_from_file(), and write_to_file
    () into mod fileutils. Moved authenticate(), check_password(), new_password(), pass_is_empty(), and  prompt_password() into passutils.
    
    This change will declutter src/main.rs and open the door to a more complex program while still having a readable file structure.
    
    Signed-off-by: BenRaz123 <ben.raz2008@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+pub mod fileutils;
+pub mod passutils;
+
+use fileutils::fileutils::*;
+use passutils::passutils::authenticate;
+
 use clap::{Parser, Subcommand};
 use requestty::Question;
-use std::fs;
-use std::io::prelude::*;
-use std::io::BufReader;
 use std::process::exit;
 
 #[derive(Parser, Debug)]
@@ -13,150 +16,6 @@ use std::process::exit;
 struct Args {
     #[command(subcommand)]
     command: Option<Subcommands>,
-}
-
-macro_rules! prompt_pass {
-    ($name:ident with message $message:expr) => {
-        let question = Question::password("$name")
-            .message($message.to_string())
-            .mask('*')
-            .build();
-        let answer = requestty::prompt_one(question).unwrap();
-        let $name = answer.as_string().unwrap().to_string();
-    };
-}
-
-fn file_is_empty(path: &str) -> bool {
-    let file = fs::OpenOptions::new().read(true).open(path).unwrap();
-    let reader = BufReader::new(&file);
-    let buf: String = reader
-        .lines()
-        .map(|l| l.unwrap())
-        .collect::<Vec<String>>()
-        .join("");
-    match &buf[..] {
-        "" | " " | "\n" => true,
-        _ => false,
-    }
-}
-
-fn pass_is_empty() -> bool {
-    let pass_file = fs::OpenOptions::new().read(true).open("./pass.txt");
-    if pass_file.is_err() {
-        return true;
-    }
-    let mut pass = String::new();
-    pass_file.unwrap().read_to_string(&mut pass).unwrap();
-    if pass.trim().is_empty() {
-        return true;
-    }
-    false
-}
-
-fn get_file_path() -> String {
-    let home = std::env::var("HOME");
-    if home.is_err() {
-        eprintln!("error: $HOME is not set!");
-        exit(1);
-    }
-    format!("{}/secrets.txt", home.unwrap())
-}
-
-fn authenticate() {
-    if pass_is_empty() {
-        new_password().unwrap();
-        return;
-    }
-    check_password(&prompt_password());
-}
-
-fn new_password() -> Result<(), Box<dyn std::error::Error>> {
-    prompt_pass!(new_pass with message "Please enter a new password");
-    prompt_pass!(rep_pass with message "Please repeat that password");
-
-    if rep_pass != new_pass {
-        println!("The two passwords do not match!");
-        exit(1);
-    }
-
-    let mut pass_file = fs::OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .create(true)
-        .open("./pass.txt")?;
-
-    pass_file.write_all(new_pass.as_bytes())?;
-
-    Ok(())
-}
-
-fn prompt_password() -> String {
-    prompt_pass!(pass with message "Please enter your password");
-    pass
-}
-
-fn check_password(pass: &str) {
-    let mut pass_file = fs::OpenOptions::new()
-        .read(true)
-        .open("./pass.txt")
-        .expect("Password file not found");
-    let mut official_pass = String::new();
-    pass_file
-        .read_to_string(&mut official_pass)
-        .expect("Could not read password file");
-    if pass != official_pass.trim() {
-        eprintln!("Incorrect password!");
-        exit(1)
-    }
-}
-
-fn write_to_file(input: &str) {
-    let mut file = fs::OpenOptions::new()
-        .read(true)
-        .create(true)
-        .append(true)
-        .open(get_file_path())
-        .unwrap();
-    writeln!(file, "{input}").unwrap();
-}
-
-fn read_from_file() -> Option<Vec<String>> {
-    let file = fs::OpenOptions::new().read(true).open(get_file_path());
-
-    if file.is_err() {
-        return None;
-    }
-
-    let mut buff = String::new();
-
-    file.unwrap().read_to_string(&mut buff).unwrap();
-
-    let output: Vec<&str> = buff.split("\n").collect::<Vec<&str>>();
-
-    let mut owned_output: Vec<String> = Vec::new();
-
-    output.iter().for_each(|x| owned_output.push(x.to_string()));
-
-    Some(owned_output)
-}
-
-fn remove_index_from_file(index: u32) -> Result<(), Box<dyn std::error::Error>> {
-    let file_read = fs::OpenOptions::new().read(true).open(get_file_path())?;
-
-    let reader = BufReader::new(&file_read);
-
-    let mut buf: Vec<String> = reader.lines().map(|l| l.unwrap()).collect();
-
-    buf.remove(index as usize);
-
-    let mut file_write = fs::OpenOptions::new()
-        .truncate(true)
-        .write(true)
-        .open(get_file_path())?;
-
-    file_write.write_all(format!("{}\n", buf.join("\n")).as_bytes())?;
-
-    Ok(())
 }
 
 fn new() {
